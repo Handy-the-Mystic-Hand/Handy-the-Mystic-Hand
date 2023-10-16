3c249dfbda6ec67606a9772fa733eaf07b34e965
CYH1107 <hildachen1107@gmail.com>
2023-10-15 21:41:44 -0400

    right hand only

--- VirtualMouse.py
+++ VirtualMouse.py
@@ -1,9 +1,8 @@
 import cv2
-import mediapipe
+from cvzone.HandTrackingModule import HandDetector
 import pyautogui
 
-hand_captured = mediapipe.solutions.hands.Hands()
-drawing = mediapipe.solutions.drawing_utils
+hand_detector = HandDetector()
 capture = cv2.VideoCapture(0)
 screen_width, screen_height = pyautogui.size()
 
@@ -11,25 +10,20 @@ while True:
     ret, frame = capture.read()
     frame = cv2.flip(frame, 1)
     frame_height, frame_width, _ = frame.shape
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    output_hand = hand_captured.process(rgb_frame)
-
-    hands = output_hand.multi_hand_landmarks
-    if hands:
-        for hand in hands:
-            drawing.draw_landmarks(frame, hand)
-            landmarks = hand.landmark
-            tip_x = int(landmarks[8].x*frame_width)
-            tip_y = int(landmarks[8].y*frame_height)
-            cv2.circle(frame, (tip_x, tip_y), 10, (0, 0, 0, 128))
-            pyautogui.moveTo(tip_x*screen_width/frame_width, tip_y*screen_height/frame_height)
-            middle_x = int(landmarks[12].x*frame_width)
-            middle_y = int(landmarks[12].y*frame_height)
-            cv2.circle(frame, (middle_x, middle_y), 10, (0, 0, 0, 128))
-            if abs(tip_x - middle_x) < 20 and abs(tip_y - middle_y) < 20:
+    hands, img = hand_detector.findHands(frame)
+    for hand in hands:
+        if hand["type"] == "Left":
+            landmarks = hand["lmList"]
+            index_tip_x = int(landmarks[8][0])
+            index_tip_y = int(landmarks[8][1])
+            pyautogui.moveTo(index_tip_x*screen_width/frame_width, index_tip_y*screen_height/frame_height)
+            middle_tip_x = int(landmarks[12][0])
+            middle_tip_y = int(landmarks[12][1])
+            if abs(index_tip_x - middle_tip_x) < 20 and abs(index_tip_y - middle_tip_y) < 20:
                 pyautogui.click()
                 pyautogui.sleep(1)
+
     cv2.imshow('current', frame)
 
     if cv2.waitKey(1) & 0xFF == 27:
