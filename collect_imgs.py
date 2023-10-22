f579b878c265fc24fe7894df160f32672b0e06ba
CYH1107 <hildachen1107@gmail.com>
2023-10-22 18:10:28 -0400

    hand gesture mouse

--- collect_imgs.py
+++ collect_imgs.py
@@ -18,7 +18,7 @@ if existing_dirs:
 else:
     start_class = 0
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 for j in range(start_class, start_class + number_of_classes):
     class_dir = os.path.join(DATA_DIR, str(j))
     if not os.path.exists(class_dir):
@@ -29,6 +29,8 @@ for j in range(start_class, start_class + number_of_classes):
     done = False
     while True:
         ret, frame = cap.read()
+
+        frame = cv2.flip(frame, 1)
         cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
                     cv2.LINE_AA)
         cv2.imshow('frame', frame)
--- inference_classifier.py
+++ inference_classifier.py
@@ -3,11 +3,12 @@ import pickle
 import cv2
 import mediapipe as mp
 import numpy as np
+import pyautogui
 
 model_dict = pickle.load(open('./model.p', 'rb'))
 model = model_dict['model']
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
@@ -15,7 +16,9 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
-labels_dict = {0: 'A', 1: 'B'}
+labels_dict = dict([(i, str(i)) for i in range(model.n_classes_)])
+
+screen_width, screen_height = pyautogui.size()
 while True:
 
     data_aux = []
@@ -23,52 +26,69 @@ while True:
     y_ = []
 
     ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
 
     H, W, _ = frame.shape
 
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     results = hands.process(frame_rgb)
+    first_hand = True
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(
-                frame,  # image to draw
-                hand_landmarks,  # model output
-                mp_hands.HAND_CONNECTIONS,  # hand connections
-                mp_drawing_styles.get_default_hand_landmarks_style(),
-                mp_drawing_styles.get_default_hand_connections_style())
+            if first_hand:
+                first_hand = False
+                mp_drawing.draw_landmarks(
+                    frame,  # image to draw
+                    hand_landmarks,  # model output
+                    mp_hands.HAND_CONNECTIONS,  # hand connections
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
 
-        for hand_landmarks in results.multi_hand_landmarks:
-            for i in range(len(hand_landmarks.landmark)):
-                x = hand_landmarks.landmark[i].x
-                y = hand_landmarks.landmark[i].y
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
 
-                x_.append(x)
-                y_.append(y)
+                    x_.append(x)
+                    y_.append(y)
 
-            for i in range(len(hand_landmarks.landmark)):
-                x = hand_landmarks.landmark[i].x
-                y = hand_landmarks.landmark[i].y
-                data_aux.append(x - min(x_))
-                data_aux.append(y - min(y_))
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x - min(x_))
+                    data_aux.append(y - min(y_))
 
-        x1 = int(min(x_) * W) - 10
-        y1 = int(min(y_) * H) - 10
+                x1 = int(min(x_) * W) - 10
+                y1 = int(min(y_) * H) - 10
 
-        x2 = int(max(x_) * W) - 10
-        y2 = int(max(y_) * H) - 10
+                x2 = int(max(x_) * W) - 10
+                y2 = int(max(y_) * H) - 10
 
-        prediction = model.predict([np.asarray(data_aux)])
+                prediction = model.predict([np.asarray(data_aux)])
 
-        predicted_character = labels_dict[int(prediction[0])]
+                predicted_character = labels_dict[int(prediction[0])]
 
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
-        cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
-                    cv2.LINE_AA)
+                if predicted_character == "0":
+                    pyautogui.click()
+                    pyautogui.sleep(1)
+
+                if predicted_character == "1":
+                    index_x = hand_landmarks.landmark[8].x * screen_width
+                    index_y = hand_landmarks.landmark[8].y * screen_height
+                    pyautogui.moveTo(index_x, index_y)
+
+                if predicted_character == "2":
+                    pyautogui.press('esc')
+
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
+                cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
+                            cv2.LINE_AA)
 
     cv2.imshow('frame', frame)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
 
 
 cap.release()
 cv2.destroyAllWindows()
+
